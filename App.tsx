3ce10d842e55d8af17bcfcd4079d1e5eb9cf679b
Carlos Aguilar <carlosaguilarpaniagua@gmail.com>
2024-08-01 11:15:47 -0600

    Elimine estilos que no deben estar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet, View } from 'react-native';
 import { Root } from "react-native-alert-notification";
 import {Provider} from "react-native-paper";
 import {NavigationContainer} from "@react-navigation/native";
@@ -16,22 +15,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-  headerStyles: {
-    color: '#fff',
-    fontSize: 20
-  }
-});
